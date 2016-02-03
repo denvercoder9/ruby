67953df3e0a99ff74eeb72ca9fba0cd21bda19c5
Fredrik <fredrik.melander@gmail.com>
2016-02-03 15:06:13 +0100

    added some more utility stuff for hashes

--- lib/hash_utils.rb
+++ lib/hash_utils.rb
@@ -1,8 +1,4 @@
-"""
-Small helper module because I kept schlepping around the rather big
-activerecord gem just for the deep_symbolize_keys! method.
-
-"""
+# Number of utility functions to include in Hash or any Hash-like class
 
 module HashUtils
   module_function
@@ -17,5 +13,47 @@ module HashUtils
 
   def deep_symbolize_keys!(hash)
     hash.replace(deep_symbolize_keys(hash))
+  end
+
+  def subhash(hash, keys)
+    hash.select { |key, value| keys.include?(key) }
+  end
+end
+
+#
+# include in Hash
+module HashExtension
+  def deep_symbolize_keys
+    _deep_symbolize_keys(self)
+  end
+
+  def deep_symbolize_keys!
+    replace(deep_symbolize_keys)
+  end
+
+  def subhash(keys)
+    select { |key, value| keys.include?(key) }
+  end
+
+  def all_keys?(keys)
+    keys.all? { |key| key?(key) }
+  end
+
+  def any_key?(keys)
+    keys.any? { |key| key?(key) }
+  end
+
+  def missing_key?(key)
+     !key?(key)
+  end
+
+  private
+
+  def _deep_symbolize_keys(hash)
+    result = hash.map do |key, value|
+      value = _deep_symbolize_keys(value) if value.class == Hash
+      [key.to_sym, value]
+    end
+    Hash[result]
   end
 end
